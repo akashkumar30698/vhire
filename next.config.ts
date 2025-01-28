413f46f496fbe45528461341b6ce971b5b8b8b82
akashkumar30698 <144131035+akashkumar30698@users.noreply.github.com>
2025-01-28 14:11:57 +0530

    fix: minor

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    // Add server/app directory to resolve modules
+    config.resolve.modules.push(path.resolve(__dirname, 'server', 'app'));
+    
+    return config;
+  },
 };
 
 export default nextConfig;
